4eae719b781b1c4a57fd07d92be6f8f64020638b
nekhoros <nekhoros@ualberta.ca>
2025-04-08 16:17:42 -0600

    + finished serial implementation

--- Serial/AntColony.py
+++ Serial/AntColony.py
@@ -19,7 +19,6 @@ def ant_colony_optimization(points, n_ants, n_iterations, alpha, beta, evaporati
     for iteration in range(n_iterations):
         paths = []
         path_lengths = []
-        
         for ant in range(n_ants):
             visited = [False]*n_points
             current_point = np.random.randint(n_points)
@@ -73,6 +72,16 @@ def ant_colony_optimization(points, n_ants, n_iterations, alpha, beta, evaporati
     ax.set_ylabel('Y Label')
     plt.show()
     
+    
+    
 # Example usage:
-points = np.random.rand(20, 2) # Generate 10 random 3D points
+points = [[565, 575],[25, 185],[345, 750],[945, 685],[845, 655],[880, 660],[25, 230],
+    [525, 1000],[580, 1175],[650, 1130],[1605, 620],[1220, 580],[1465, 200],[1530, 5],
+    [845, 680],[725, 370],[145, 665],[415, 635],[510, 875],[560, 365],[300, 465],
+    [520, 585],[480, 415],[835, 625],[975, 580],[1215, 245],[1320, 315],[1250, 400],
+    [660, 180],[410, 250],[420, 555],[575, 665],[1150, 1160],[700, 580],[685, 595],
+    [685, 610],[770, 610],[795, 645],[720, 635],[760, 650],[475, 960],[95, 260],
+    [875, 920],[700, 500],[555, 815],[830, 485],[1170, 65],[830, 610],[605, 625],
+    [595, 360],[1340, 725],[1740, 245]]
+points = np.array(points)
 ant_colony_optimization(points, n_ants=10, n_iterations=100, alpha=1, beta=1, evaporation_rate=0.5, Q=1)
--- Serial/ConvertFileTo2dArray.py
+++ Serial/ConvertFileTo2dArray.py
@@ -1,11 +1,12 @@
-file = open(r"Data\UsCapitals.txt", "r")
 
-print("{")
+file = open(r"Data/HamilitonianCycle.txt", "r")
+
+print("{", end="")
 oneLine = file.readlines()
 for newLine in oneLine:
-    if(newLine != "EOF"):
+    if newLine != "EOF" or newLine == "-1\n":
         print("{", end="")
-        newTxt = newLine[:-1].split(" ")
+        newTxt = newLine[:-1].split()
         print(newTxt[0], end="")
         print(", ", end="")
         print(newTxt[1], end="")
